75db13dc11cec5c03f837e568244212983ce6c6b
dfhelloworld <dfhelloworld@126.com>
2018-12-29 21:18:23 +0800

    增加天气图标

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -2,85 +2,72 @@
 var qcloud = require('../../vendor/wafer2-client-sdk/index')
 var config = require('../../config')
 var util = require('../../utils/util.js')
-var QQMapWX = require('../../utils/qqmap-wx-jssdk.js')
 
 Page({
-    data: {
-        userInfo: {},
-        logged: false,
-        takeSession: false,
-        requestResult: '',
-        inputValue: '',
-        qqmap:{}
-    },
-    onLoad:function(){
-       this.qqmap = new QQMapWX({
-        key: '4WJBZ-FXP6J-TFJF4-FWJMM-BJKOO-4ZFDX'
-      }) 
-
-      this.getLocation().then(result => {
-        if (result) {
-          let latitude = result.latitude;
-          let longitude = result.longitude;
-          return this.getCityInfo(latitude, longitude);
-        } else {
-          throw '无法获取地理位置'
-        }
-      }).then(res => {
-
-      }).catch(err => {
-        console.log(err);
-      })
-    },
-    onShow:function(){
- 
-    },
-    //获取当前位置信息
-    getLocation:function(){ 
-        return new Promise((reslove,reject)=>{
-        wx.getLocation({
-            type: 'wgs84',
-            success:reslove, 
-            fail:reject 
-        }) 
-    })},
-
-    //根据经纬度获取城市信息
-    getCityInfo:function(latitude,longitude){
-      return new Promise((resolve,reject)=>{
-        this.qqmap.reverseGeocoder({
-          location:{
-            latitude:latitude,
-            longitude:longitude
-          },
-          success:resolve,
-          fail:reject
-        })
-      })
-    },
-  
- 
-  bindButtonTap:function(){  
-    if(this.data.inputValue){
-      let url = encodeURI('https://free-api.heweather.net/s6/weather/forecast?location=' + this.data.inputValue + '&key=2be22d4891af4199aaff57f82a9eec9a')
-      console.log(url);
-      wx.request({
-        url: url, 
-        success(result) {
-          let tommorrow = result.data.HeWeather6[0].daily_forecast[0]
-          console.log(tommorrow)
-          util.showModel(`明日天气:`,`${tommorrow.cond_txt_d},最低温度：${tommorrow.tmp_min},最高温度:${tommorrow.tmp_max}`) 
-        },
-
-        fail(error) {
-          util.showModel('请求失败', error)
-          console.log('request fail', error)
-        }
-      })
-    }
+  data: {
+    inputValue: '',
+    currentCity: "",
+    weatherInfo: [],
+    todayWeather: {},
+    mainImageSrc: "",
+    secondImageSrc: "",
+    thirdImageSrc: ""
   },
-  cityInput:function(e){
-    this.setData({ inputValue: e.detail.value} 
-    )
-  }, 
+  //初始化数据
+  onLoad: function() {
+    this.getLocation().then(result => {
+      if (result) {
+        let latitude = result.latitude;
+        let longitude = result.longitude;
+        return this.getWeatherInfo(latitude, longitude);
+      } else {
+        throw '无法获取地理位置'
+      }
+    }).then(res => {
+
+      if (res.data.HeWeather6.length) {
+        this.weatherInfo = res.data.HeWeather6[0].daily_forecast;
+        this.todayWeather = this.weatherInfo[0];
+        this.setData({
+          todayWeather: this.weatherInfo[0],
+          tommorrowWeather: this.weatherInfo[1],
+          dayAfterWeather: this.weatherInfo[2],
+          mainImageSrc: `../../weather-icon/${this.todayWeather.cond_code_d}.png`,
+          secondImageSrc: `../../weather-icon/${this.weatherInfo[1].cond_code_d}.png`,
+          thirdImageSrc: `../../weather-icon/${this.weatherInfo[2].cond_code_d}.png`,
+        })
+        console.log(this.todayWeather)
+      }
+      this.setData({
+        currentCity: res.data.HeWeather6[0].basic.parent_city
+      })
+    }).catch(err => {
+      console.log(err);
+    })
+  },
+  // onReady: function() {
+  //   console.log(this.weatherInfo)
+  // },
+  //获取当前位置信息
+  getLocation: function() {
+    return new Promise((reslove, reject) => {
+      wx.getLocation({
+        type: 'wgs84',
+        success: reslove,
+        fail: reject
+      })
+    })
+  },
+
+  //获取天气信息
+  getWeatherInfo: function(latitude, longitude) {
+    let url = encodeURI(`https://free-api.heweather.net/s6/weather/forecast?location= ${latitude},${longitude}&key=2be22d4891af4199aaff57f82a9eec9a`);
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: url,
+        success: resolve,
+        fail: reject
+      })
+    })
+  },
 })
